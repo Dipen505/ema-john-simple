6aaeefec2ef588fc544c37b7b123a57f236c444d
Dipen505 <dipenchakma505@gmail.com>
2021-12-20 19:35:30 +0600

    Header components complete

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../images/logo.png';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <div className='header'><img src= {logo} alt="" /></div>
+            <div className='nav-bar'>
+                <nav>
+                    <a href="/shop">Shop</a>
+                    <a href="/review">Order Review</a>
+                    <a href="manage">Manage Inventory</a>
+                    </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
